9aaf53d2657aa7b4723f8319f822713d78c498f4
MikeSanDev <mjs070594@gmail.com>
2023-11-22 14:17:08 -0600

    first commit

---
+++ multiclipboard.py
@@ -0,0 +1,9 @@
+#can save things on a  clipboard, can display list and load things from the list
+#commands: save, load, list
+#preinstalled modules - part of Python module
+import sys
+import clipboard
+
+import json
+
+
