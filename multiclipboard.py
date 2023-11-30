8861bc7d96db7fb4dc58011d61e33e6ee524f93f
MikeSanDev <mjs070594@gmail.com>
2023-11-30 11:05:22 -0600

    saved last copy on clipboard to json file

--- multiclipboard.py
+++ multiclipboard.py
@@ -13,9 +13,12 @@ def save_data(filepath, data):
         json.dump(data, f) #write python dictionary as a json object
 
 def load_data(filepath):
-    with open(filepath, "r") as f:
-        data = json.load(f)
-        return data #returns python dictionary representation of this json object
+    try: #if filepath fails/ no file exists then it goes to except:
+        with open(filepath, "r") as f:
+            data = json.load(f)
+            return data #returns python dictionary representation of this json object
+    except: 
+        return {} #the exception returns an empty dictionary
 
 if len(sys.argv) == 2:
     command = sys.argv[1]
@@ -25,6 +28,7 @@ if len(sys.argv) == 2:
         key = input("Enter a key:")
         data[key] = clipboard.paste() #pastes data onto clipboard
         save_data(SAVED_DATA, data) #will rewrite json file with whatever was pasted on the clipboard
+        print("Input saved successfully!")
     elif command == "load":
         print('load')
     elif command == "list":
